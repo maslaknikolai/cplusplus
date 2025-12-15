20c7d8352e4fca00cdd4beb1372c36e75e4bf4e0
Nikolai Maslak <nikomaslak@gmail.com>
2025-12-14 22:19:03 -0800

    overloaded funcs

--- main.cpp
+++ main.cpp
@@ -3,12 +3,18 @@
 #include <cmath>
 
 void foo(std::string name, int age);
+void foo(std::string name);
 
 int main() {
     std::string v = "n";
+    foo(v);
     foo(v, (int)12);
 
     return 0;
+}
+
+void foo(std::string aname) {
+    std::cout << aname;
 }
 
 void foo(std::string aname, int age) {
